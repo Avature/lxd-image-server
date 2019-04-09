c384dec34ee9ca140ba2f19cf9baac9bad4401f1
Carlos Duelo <carlos.duelo@avature.net>
2019-04-09 15:32:16 -0500

    License added. Remove references to avature. No keys no passwords. Add travis support.

--- setup.py
+++ setup.py
@@ -12,13 +12,15 @@ def get_version_from_debian_changelog():
 setup(
     name='lxd-image-server',
     version=get_version_from_debian_changelog(),
-    author='David Burgos',
-    author_email='david.burgos@avature.net',
-    url='https://gitlab.xcade.net/david.burgos/lxd-image-server',
+    license='Apache License 2.0',
+    author='Avature',
+    author_email='devops@avature.net',
+    url='https://github.com/avature/lxd-image-server',
     description='Package to create and manage a simplestreams'
                 'lxd image server on top of nginx.',
+    long_description=io.open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
     install_requires=open('requirements.txt').read().splitlines(),
-    license='Propietary',
     entry_points={
         'console_scripts': ['lxd-image-server = lxd_image_server.cli:main']
     },
@@ -26,7 +28,11 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
         'Topic :: Internet :: WWW/HTTP :: HTTP Servers'
     ]
